8dab6e99780822d24d0acb8cfbcb2a7ae3ff27ca
adriann020 <adrian.florin.mano@gmail.com>
2026-04-07 00:40:33 +0300

    Update Effects.cpp

--- src/Effects.cpp
+++ src/Effects.cpp
@@ -1,21 +1,20 @@
 #include "headers/Comp.h"
 
-
 void DataEngine::MoveTextContinue() {
 
     float speedText = 0.8 * (deltaTime / 16); 
 
-    if(this->coordinates->contRect.y != 700 && this->gameState->turntextcont == false ) {
+    if(this->coordinates->contRect.y != 700 && this->effectState->turntextcont == false ) {
         this->coordinates->contRect.y -= speedText; 
         if(this->coordinates->contRect.y  <= 700 ) {
-            this->gameState->turntextcont = true; 
+            this->effectState->turntextcont = true; 
         }
     }
 
-    if(this->gameState->turntextcont == true) {
+    if(this->effectState->turntextcont == true) {
         this->coordinates->contRect.y += speedText;
         if(this->coordinates->contRect.y >= 750) {
-            this->gameState->turntextcont = false; 
+            this->effectState->turntextcont = false; 
         }
     }
     
