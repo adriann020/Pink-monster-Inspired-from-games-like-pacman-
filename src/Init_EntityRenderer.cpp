fb42d53953c481e033331190f8d86398c2bd628d
adriann020 <adrian.florin.mano@gmail.com>
2026-04-11 17:44:50 +0300

    Update Init_EntityRenderer.cpp

--- src/Init_EntityRenderer.cpp
+++ src/Init_EntityRenderer.cpp
@@ -1,32 +1,32 @@
 #include "headers/Comp.h"
 
-void DataEngine::RenderTextureRotated() { // Char
+void GameManager::RenderTextureRotated() { // Char
     if(this->gameState->fix){
         // could've done NULL instead of this->..->rotationAngle
          SDL_RenderTextureRotated(renderer, this->init_texture->get_imageTextureChar(), NULL, &this->coordinates->destRect, this->gameState->rotationAngle, NULL, SDL_FLIP_HORIZONTAL);
     }else  SDL_RenderTextureRotated(renderer, this->init_texture->get_imageTextureChar(), NULL, &this->coordinates->destRect, this->gameState->rotationAngle, NULL, SDL_FLIP_NONE);
 }
 
-void DataEngine::RenderCharacter() {  //Char unused
+void GameManager::RenderCharacter() {  //Char unused
 
     SDL_RenderTexture(renderer, this->init_texture->get_imageTextureChar(), NULL, &this->coordinates->destRect);
 }
 
 // RenderNPC
 
-void DataEngine::RenderNPC(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &srcreect ,SDL_FRect &coord, SDL_FlipMode &flip){
+void GameManager::RenderNPC(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &srcreect ,SDL_FRect &coord, SDL_FlipMode &flip){
     SDL_RenderTextureRotated(rend, texture, &srcreect, &coord, 0, NULL, flip);
 }
 
 // RenderImportantThings
 
-void DataEngine::RenderImportantThings(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &coord) {
+void GameManager::RenderImportantThings(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &coord) {
     SDL_RenderTexture(rend, texture, NULL, &coord); 
 }
 
 // RenderCoin
 
-void DataEngine::RenderCoin(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &coord) {
+void GameManager::RenderCoin(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &coord) {
     SDL_RenderTexture(rend, texture, NULL , &coord);
 }
 
