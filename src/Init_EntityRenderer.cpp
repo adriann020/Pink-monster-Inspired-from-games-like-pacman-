0c0d51012e8c055fac8659d9f1977516da75e2ef
adriann020 <adrian.florin.mano@gmail.com>
2026-04-12 22:40:51 +0300

    Update Init_EntityRenderer.cpp

--- src/Init_EntityRenderer.cpp
+++ src/Init_EntityRenderer.cpp
@@ -1,32 +1,19 @@
 #include "headers/GameManager.h"
 
-void GameManager::RenderTextureRotated() { // Char
+// Render Char (used for idle in MovementChar function and more)
+void GameManager::RenderTextureRotated() { 
     if(this->playerState->fix){
         // could've done NULL instead of this->..->rotationAngle
          SDL_RenderTextureRotated(renderer, this->init_texture->get_imageTextureChar(), NULL, &this->coordinates->destRect, this->playerState->rotationAngle, NULL, SDL_FLIP_HORIZONTAL);
     }else  SDL_RenderTextureRotated(renderer, this->init_texture->get_imageTextureChar(), NULL, &this->coordinates->destRect, this->playerState->rotationAngle, NULL, SDL_FLIP_NONE);
 }
 
-void GameManager::RenderCharacter() {  //Char unused
-
-    SDL_RenderTexture(renderer, this->init_texture->get_imageTextureChar(), NULL, &this->coordinates->destRect);
-}
-
 // RenderNPC
-
 void GameManager::RenderNPC(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &srcreect ,SDL_FRect &coord, SDL_FlipMode &flip){
     SDL_RenderTextureRotated(rend, texture, &srcreect, &coord, 0, NULL, flip);
 }
 
-// RenderImportantThings
-
-void GameManager::RenderImportantThings(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &coord) {
+// Render everything else (Coins, Map, fightmap, play, gameovermap)
+void GameManager::RenderThings(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &coord) {
     SDL_RenderTexture(rend, texture, NULL, &coord); 
 }
-
-// RenderCoin
-
-void GameManager::RenderCoin(SDL_Renderer *&rend, SDL_Texture *&texture, SDL_FRect &coord) {
-    SDL_RenderTexture(rend, texture, NULL , &coord);
-}
-
