5b063792e7505ddc818f768a9e25b3381a74a6e4
adriann020 <adrian.florin.mano@gmail.com>
2026-04-13 20:33:31 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char** argv) {
 
     /**
 	  * Make a new asset pak with -> execute();
-	  * Call it in the main thread.
     */
 
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO); // Video, audio
